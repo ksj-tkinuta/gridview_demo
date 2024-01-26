58095855f9422176841ca17a61650a9c05cef827
t.kinuta <takashi.kinuta@bluetec.co.jp>
2024-01-26 10:22:33 +0900

    add GridView

---
+++ android/app/src/main/kotlin/com/example/gridview_demo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.gridview_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+import 'gridview_page.dart';
+
+void main() => runApp(GridviewDemo());
+
